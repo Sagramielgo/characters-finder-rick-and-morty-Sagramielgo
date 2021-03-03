27e960fd637d60cc29a514b32d39cec23a35b18d
Sagramielgo <sagramielgo@gmail.com>
2021-03-03 14:39:07 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import '../stylesSheets/App.scss';
+import React from 'react';
+
+const App = () => {
+  return <h1>Sagra es la mejor!</h1>;
+};
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* import './stylesSheets/index.scss'; */
+import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
