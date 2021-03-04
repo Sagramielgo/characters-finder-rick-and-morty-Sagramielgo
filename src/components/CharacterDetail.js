efc1deed507db523942f035c3262d5f594a289c0
Sagramielgo <sagramielgo@gmail.com>
2021-03-04 11:02:01 +0100

    add gif to error message and update Detail styles

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -2,30 +2,28 @@ import React from 'react';
 /* import PropTypes from 'prop-types'; */
 import '../stylesSheets/CharacterDetail.scss';
 import { Link } from 'react-router-dom';
-/* import image from '../images/meeseeks.png'; */
+import image from '../images/CatNoWay.gif';
 
 const CharacterDetail = (props) => {
   if (props.characterInfo === undefined) {
     return (
       <div className="modal">
         <div className="modal__dialog">
-          <div className="modal__dialog">
-            <div className="modal__content">
-              <header className="modal__header">
-                <Link to="/">
-                  <span className="modal__close icon fas fa-undo"></span>
-                </Link>
-              </header>
-              <div className="modal__text-error">
-                <p>Error: character not found</p>
-              </div>
-              {/*     <div className="modal__img">
-                <img
-                  className="modal__img-error"
-                  src={image}
-                  alt={'Meeseeks worrying face'}
-                />
-              </div> */}
+          <div className="modal__content">
+            <header className="modal__header">
+              <Link to="/">
+                <span className="modal__close icon fas fa-undo"></span>
+              </Link>
+            </header>
+            <div className="modal__text-error">
+              <p>Error: character not found</p>
+            </div>
+            <div className="modal__img">
+              <img
+                className="modal__img-error"
+                src={image}
+                alt={'Cat no way'}
+              />
             </div>
           </div>
         </div>
@@ -33,45 +31,42 @@ const CharacterDetail = (props) => {
     );
   } else {
     return (
-      <div className="modal">
+      <article className="modal">
         <div className="modal__dialog">
-          <div className="modal__dialog">
-            <div className="modal__content">
-              <header className="modal__header">
-                <Link to="/">
-                  <span className="modal__close icon fas fa-undo"></span>
-                </Link>
-              </header>
-              <div className="modal__img">
-                <img
-                  className="modal__img-src"
-                  src={props.characterInfo.image}
-                  alt={`${props.characterInfo.name}'s portrait`}
-                />
-              </div>
-              <div className="modal__text">
-                <h2>{props.characterInfo.name}</h2>
-                <p>
-                  <span className="modal__text--property">Status: </span>
-                  <span>{props.characterInfo.status}</span>
-                </p>
-                <p>
-                  <span className="modal__text--property">Species: </span>
-                  <span>{props.characterInfo.specie}</span>
-                </p>
-                <p>
-                  <span className="modal__text--property">Origin: </span>
-                  <span>{props.characterInfo.planet}</span>
-                </p>
-                <p>
-                  <span className="modal__text--property">Episodes: </span>
-                  <span>{props.characterInfo.episodes.length}</span>
-                </p>
-              </div>
+          <div className="modal__content">
+            <header className="modal__header"></header>
+            <div className="modal__img">
+              <img
+                className="modal__img-src"
+                src={props.characterInfo.image}
+                alt={`${props.characterInfo.name}'s portrait`}
+              />
             </div>
+            <div className="modal__text">
+              <h2>{props.characterInfo.name}</h2>
+              <p>
+                <span className="modal__text--property">Status: </span>
+                <span>{props.characterInfo.status}</span>
+              </p>
+              <p>
+                <span className="modal__text--property">Species: </span>
+                <span>{props.characterInfo.specie}</span>
+              </p>
+              <p>
+                <span className="modal__text--property">Origin: </span>
+                <span>{props.characterInfo.planet}</span>
+              </p>
+              <p>
+                <span className="modal__text--property">Episodes: </span>
+                <span>{props.characterInfo.episodes.length}</span>
+              </p>
+            </div>
+            <Link to="/">
+              <span className="modal__close icon iconDetail fas fa-undo"></span>
+            </Link>
           </div>
         </div>
-      </div>
+      </article>
     );
   }
 };
