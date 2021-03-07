4455216ca82045da5a2c02731b76dbde94a39a32
Sagramielgo <sagramielgo@gmail.com>
2021-03-07 19:13:30 +0100

    small improvements and review

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,22 @@ import '../stylesSheets/Footer.scss';
 //FUNCTIONAL COMPONENT
 const Footer = () => {
   return (
-    <a
-      className=""
-      href="https://github.com/Sagramielgo"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <h5 className="copy animate__animated animate__headShake animate__infinite">
-        By #Git Funtastic &nbsp;
-        <span>
-          <i className="fas fa-paw "></i>
-        </span>
-        &nbsp;2021
-      </h5>
-    </a>
+    <section className="Footer">
+      <a
+        className=""
+        href="https://github.com/Sagramielgo"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h5 className="copy animate__animated animate__headShake animate__infinite">
+          By #Git Funtastic &nbsp;
+          <span>
+            <i className="fas fa-paw "></i>
+          </span>
+          &nbsp;2021
+        </h5>
+      </a>
+    </section>
   );
 };
 export default Footer;
